9dd98362a1fd6697e27e17d58825097d004b3494
Faton Hoti <hotifaton@outlook.com>
2021-12-05 20:28:35 +0100

    Update day 5

--- 2021/python/day5/day5.py
+++ 2021/python/day5/day5.py
@@ -1,15 +1,3 @@
-def get_covered_points_diagonally(start, end):
-    covered_points = []
-    k = 1 if (end[1] - start[1]) / (end[0] - start[0]) > 0 else -1
-    x = start[0]
-    y = start[1]
-    for _ in range(abs(start[1] - end[1]) + 1):
-        covered_points.append((x, y))
-        x += 1
-        y += k
-    return covered_points
-
-
 def update_points(points, new_points):
     for point in new_points:
         if point not in points:
@@ -22,19 +10,24 @@ def calculate(*, segments, do_part_2=False):
     for segment in segments:
         p1 = segment[0]  # (x1, y1)
         p2 = segment[1]  # (x2, y2)
-        if p1[0] == p2[0]:  # Vertical line
+        covered_points = []
+        if p1[0] == p2[0]:  # Vertical segment
             if p1[1] > p2[1]:  # y1 > y2?
                 p1, p2 = p2, p1
             covered_points = [(p1[0], y) for y in range(p1[1], p2[1] + 1)]
-            update_points(points, covered_points)
-        elif p1[1] == p2[1]:  # Horizontal line
+        elif p1[1] == p2[1]:  # Horizontal segment
             if p1[0] > p2[0]:  # x1 > x2?
                 p1, p2 = p2, p1
             covered_points = [(x, p1[1]) for x in range(p1[0], p2[0] + 1)]
-            update_points(points, covered_points)
         elif do_part_2:  # Diagonal segment
-            covered_points = get_covered_points_diagonally(p1, p2)
-            update_points(points, covered_points)
+            k = 1 if (p2[1] - p1[1]) / (p2[0] - p1[0]) > 0 else -1
+            x = p1[0]
+            y = p1[1]
+            for _ in range(abs(p1[1] - p2[1]) + 1):
+                covered_points.append((x, y))
+                x += 1
+                y += k
+        update_points(points, covered_points)
 
     return sum(1 for _ in filter(lambda p: points[p] >= 2, points))
 
