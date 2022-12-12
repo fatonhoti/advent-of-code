4e4ffc80a336109d00c52750bddad76602767a78
Faton Hoti <hotifaton@outlook.com>
2022-12-12 10:05:23 +0100

    [2022] day 12

--- 2022/python/day_11/day_11.py
+++ 2022/python/day_11/day_11.py
@@ -1,6 +1,4 @@
 from collections import deque
-from copy import deepcopy
-
 
 def run():
     day_n = __file__.split("\\")[-1][:-3]
@@ -19,33 +17,21 @@ def run():
                 "test": lambda item, comparison=f"{true} if item % {divisor} == 0 else {false}": eval(comparison),
             }
             monkeys.append(monkey_info)
-        monkeys_2 = deepcopy(monkeys)
 
-    # Part 1
-    for _ in range(20):
+    # Part 1 & 2
+    LCM = 13 * 2 * 19 * 11 * 7 * 5 * 3 * 17 * 23
+    part_1 = False
+    for _ in range(20 if part_1 else 10_000):
         for monkey in monkeys:
             while monkey["items"]:
                 monkey["inspected"] += 1
                 item = monkey["items"].popleft()
-                new = monkey["op"](item) // 3
+                new = monkey["op"](item) 
+                new = new // 3 if part_1 else new % LCM
                 to = monkey["test"](new)
                 monkeys[to]["items"].append(new)
     inspections = sorted([monkey["inspected"] for monkey in monkeys], reverse=True)
-    print(f"Part 1: {inspections[0] * inspections[1]}")
-
-    # Part 2
-    LCM = 13 * 2 * 19 * 11 * 7 * 5 * 3 * 17 * 23
-    for _ in range(10000):
-        for monkey in monkeys_2:
-            while monkey["items"]:
-                monkey["inspected"] += 1
-                item = monkey["items"].popleft()
-                new = monkey["op"](item) % LCM
-                to = monkey["test"](new)
-                monkeys_2[to]["items"].append(new)
-    inspections = sorted([monkey["inspected"] for monkey in monkeys_2], reverse=True)
-    print(f"Part 2: {inspections[0] * inspections[1]}")
-
+    print(f"Part n: {inspections[0] * inspections[1]}")
     """
     Part 1: 151312
     Part 2: 51382025916
---
+++ 2022/python/day_12/day_12.py
@@ -0,0 +1,84 @@
+from collections import deque
+from string import ascii_lowercase
+
+heights = {c: i for i, c in enumerate(ascii_lowercase)}
+heights["S"] = 0
+heights["E"] = 25
+
+
+def neighbours(grid, cell):
+    nbs = []
+    r, c = cell
+    dr = [-1, 0, 1, 0]
+    dc = [0, 1, 0, -1]
+    for i in range(4):
+        rt = r + dr[i]
+        ct = c + dc[i]
+        if (
+            ct in range(0, len(grid[0]))
+            and rt in range(0, len(grid))
+            and heights[grid[rt][ct]] <= heights[grid[r][c]] + 1
+        ):
+            nbs.append((rt, ct))
+    return nbs
+
+
+def bfs(grid, start, GOAL):
+    # Iterative BFS
+    Q = deque()
+    Q.append((start, []))  # [start] for adding start node too
+    visited = set()
+    visited.add(start)
+    while Q:
+        node, path = Q.popleft()
+        if (node[0], node[1]) == GOAL:
+            return path  # + [node] for adding end node too
+        for nb in neighbours(grid, node):
+            if nb not in visited:
+                visited.add(nb)
+                Q.append((nb, path + [nb]))
+    # No path to goal found
+    return None
+
+
+def run():
+    day_n = __file__.split("\\")[-1][:-3]
+    with open(f"{day_n}.txt", "r") as f:
+
+        start = None
+        end = None
+        a_cells = []  # Used in part 2
+        grid = []
+        for r, row in enumerate(f.read().split("\n")):
+            rr = []
+            for c, cell in enumerate(row):
+                if cell == "S":
+                    start = (r, c)
+                elif cell == "E":
+                    end = (r, c)
+                elif cell == "a":
+                    a_cells.append((r, c))
+                rr.append(cell)
+            grid.append(rr)
+
+    # Part 1
+    path = bfs(grid, start, end)
+    print(f"Part 1: {len(path)}")
+
+    # Part 2
+    len_paths = []
+    for starting_cell in a_cells:
+        path = bfs(grid, starting_cell, end)
+        if path:
+            len_paths.append(len(path))
+    print(f"Part 2: {min(len_paths)}")
+
+    """
+    Part 1: 408
+    Part 2: 399
+    """
+
+
+if __name__ == "__main__":
+    run()
+    
