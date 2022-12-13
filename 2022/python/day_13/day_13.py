4938d4b49360941965bc9ec05fa6bc45d34c88ca
Faton Hoti <hotifaton@outlook.com>
2022-12-13 13:24:58 +0100

    [2022] Update solution for day 13

--- 2022/python/day_13/day_13.py
+++ 2022/python/day_13/day_13.py
@@ -1,21 +1,3 @@
-def quicksort(array):
-
-    if len(array) < 2:
-        return array
-
-    low, same, high = [], [], []
-    pivot = array[(len(array) - 1) // 2]
-    for item in array:
-        if compare(item, pivot) is None:
-            same.append(item)
-        elif compare(item, pivot):
-            low.append(item)
-        else:
-            high.append(item)
-
-    return quicksort(low) + same + quicksort(high)
-
-
 def compare(a, b):
 
     a = a[::-1]
@@ -26,25 +8,24 @@ def compare(a, b):
         if isinstance(left, int) and isinstance(right, int):
             if left == right:
                 continue
-            return left < right
+            return -1 if left < right else 1
         elif isinstance(left, list) and isinstance(right, int):
             right = [right]
         elif isinstance(left, int) and isinstance(right, list):
             left = [left]
-        res = compare(left, right)
-        if res is not None:
+        if res := compare(left, right):
             return res
 
     if len(a) == 0 and len(b) > 0:
         # Left list ran out of items first, right order!
-        return True
+        return -1
 
     if len(a) > 0 and len(b) == 0:
         # Right list ran out of items first, incorrect order!
-        return False
+        return 1
 
     # Could not decide
-    return None
+    return 0
 
 
 def run():
@@ -59,13 +40,14 @@ def run():
     pair = 1
     for i in range(0, len(packets) - 2, 2):
         a, b = packets[i : i + 2]
-        if compare(a, b):
+        if compare(a, b) == -1:
             s += pair
         pair += 1
     print(f"Part 1: {s}")
 
     # Part 2
-    p = quicksort(packets + [[[2]]] + [[[6]]])
+    from functools import cmp_to_key
+    p = sorted(packets + [[[2]]] + [[[6]]], key=cmp_to_key(compare))
     i = p.index([[2]]) + 1
     j = p.index([[6]]) + 1
     print(f"Part 2: {i*j}")
