78b4237dee0b1e6db6abb02cddb0f293359abfd7
Faton Hoti <85518265+fatonhoti@users.noreply.github.com>
2024-12-02 16:33:34 +0100

    Update day 2

--- 2024/python/day_2/day_2.py
+++ 2024/python/day_2/day_2.py
@@ -1,58 +1,35 @@
 def is_report_safe(report):
     is_increasing = report == sorted(report)
     is_decreasing = report == sorted(report, reverse=True)
-    valid_diffs = True
+    diffs_ok = True
     for i in range(len(report) - 1):
-        this = report[i]
-        next = report[i + 1]
-        diff = abs(this - next)
+        diff = abs(report[i] - report[i + 1])
         if diff < 1 or diff > 3:
-            valid_diffs = False
+            diffs_ok = False
             break
 
-    return (is_decreasing or is_increasing) and valid_diffs
+    return (is_decreasing or is_increasing) and diffs_ok
 
 
 def try_fix_report(report):
-    for i in range(0, len(report) - 1):
-        this = report[i]
-        next = report[i + 1]
-        diff = abs(this - next)
-
-        # potential problems
-        is_invalid_diff = (
-            diff < 1 or diff > 3
-        )  # difference between the current level and the next is not in [1, 3]
-        is_invalid_increasing = (
-            next <= this
-        )  # next element is smaller, supposed to be bigger
-        is_invalid_decreasing = (
-            next >= this
-        )  # next element is bigger, supposed to be smaller
-
-        # found a problem? remove it and see if the result is safe
-        if is_invalid_diff or is_invalid_increasing or is_invalid_decreasing:
-            this_removed = report[:i] + report[i + 1:]
-            next_removed = report[: i + 1] + report[i + 2:]
-            if is_report_safe(this_removed) or is_report_safe(next_removed):
-                return True
+    for i in range(0, len(report)):
+        this_removed = report[:i] + report[i + 1:]
+        if is_report_safe(this_removed):
+            return True
 
 
 def run():
     day_n = __file__.split("\\")[-1][:-3]
     with open(f"{day_n}.txt", "r") as f:
-        reports = []
-        for level in f.readlines():
-            reports.append(list(map(int, level.split())))
+        reports = [list(map(int, level.split())) for level in f.readlines()]
 
     part1 = 0
     part2 = 0
     for report in reports:
-        if not is_report_safe(report):
-            if try_fix_report(report):
-                part2 += 1
-        else:
+        is_safe = is_report_safe(report)
+        if is_safe:
             part1 += 1
+        if try_fix_report(report):
             part2 += 1
 
     print(f"Part 1: {part1}")
